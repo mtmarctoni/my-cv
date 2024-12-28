cb7dce95d7d7cb886093d0e7e4399a59ec4e26e2
mtmarctoni <marctonimas@protonmail.com>
2024-12-28 21:11:14 +0100

    Updates

---
+++ icons/index.js
@@ -0,0 +1,10 @@
+// src/icons/index.js
+import Github from './github-mark.svg?component'
+import GithubWhite from './github-mark-white.svg?component'
+import LinkedIn from './linkedin-mark.svg?component'
+
+export {
+    Github,
+    GithubWhite,
+    LinkedIn
+}
